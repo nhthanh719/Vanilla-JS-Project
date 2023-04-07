c9afff9ae0bb955dcddc389723b1e7c423601610
Thành Nguyễn <nhthanh719@gmail.com>
2023-04-07 22:10:40 +0700

    update JS

--- calculator.js
+++ calculator.js
@@ -1,14 +1,45 @@
+let buffer = "0";
+
+const screen = document.querySelector("#screen");
+
 function buttonClick(value) {
-    console.log(value);
+  if (isNaN(parseInt(value)) === true) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerenderScreen();
 }
 
+function handleNumber(number) {
+  if (buffer === '0') {
+    buffer = number;
+  } else {
+    buffer += number;
+  }
 
+  console.log(`buffer now is ${buffer}`);
+}
+
+function handleSymbol(symbol) {
+  if (symbol === 'C') {
+    buffer = '0';
+    rerenderScreen();
+  } elseif (symbol === '=') {
+      
+  }
+}
 
 function init() {
-  document.querySelector('#buttons')
-    .addEventListener('click', function(event) {
-        buttonClick(event.target.innerText);
+  document
+    .querySelector("#buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
     });
+}
+
+function rerenderScreen() {
+  screen.innerText = buffer;
 }
 
 init();
