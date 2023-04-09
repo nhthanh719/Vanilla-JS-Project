0e79dcfc71e57e41827910a0fec50ad54499188b
Thành Nguyễn <nhthanh719@gmail.com>
2023-04-09 16:08:53 +0700

    Fix multiply & divide operator
    Fix issue #6 when take a number, then click multiply/divide and immediate click equal will results in 0. Now it will multiply or divide by itself

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ let previousOperator = null;
 const screen = document.querySelector("#screen");
 
 function buttonClick(value) {
-  if (isNaN(parseInt(value))) {
+  if (isNaN(parseFloat(value))) {
     handleSymbol(value);
   } else {
     handleNumber(value);
@@ -13,59 +13,85 @@ function buttonClick(value) {
 }
 
 function handleNumber(number) {
-  if (buffer === '0') {
+  if (buffer === "0") {
     buffer = number;
   } else {
     buffer += number;
   }
+  buffer = parseFloat(buffer).toString();
 }
 
 function handleSymbol(symbol) {
   switch (symbol) {
-    case 'C':
-      buffer = '0';
-      previousOperator = null;
-      break;
-
-    case '=':
-      if (previousOperator === null) {
-        //Need numbers to do math
-        return;
-      } else {
-
-        calculate(parseInt(buffer));
-        buffer = "" + runningTotal; 
-      } 
-      
+    case "C":
+      buffer = "0";
       runningTotal = 0;
       previousOperator = null;
       break;
 
+    case "=":
+      if (previousOperator === null) {
+        // Need numbers to do math
+        return;
+      }
+      console.log(runningTotal);
+      calculate(parseFloat(runningTotal));
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      previousOperator = null;
+      break;
 
-    case '←':
+    case "←":
       if (buffer.length === 1) {
-        buffer = '0';
+        buffer = "0";
       } else {
         buffer = buffer.substring(0, buffer.length - 1);
       }
       break;
 
-    case '+':
-    case '-':
-    case 'x':
-    case '÷':
-      handleMath(symbol);
-      buffer = '0';
+    case "+":
+    case "-":
+      if (previousOperator === null) {
+        previousOperator = symbol;
+        runningTotal = parseFloat(buffer);
+      } else {
+        calculate(parseFloat(buffer));
+        previousOperator = symbol;
+      }
+      buffer = "0";
+      break;
+
+    case "x":
+    case "÷":
+      if (previousOperator === null) {
+        previousOperator = symbol;
+        runningTotal = parseFloat(buffer);
+      } else {
+        // Check if a number has been entered before clicking the operator
+        if (buffer === "0") {
+          previousOperator = symbol;
+        } else {
+          calculate(parseFloat(buffer));
+          previousOperator = symbol;
+        }
+      }
+      buffer = "0";
       break;
   }
 }
 
 function handleMath(symbol) {
-  if (buffer === '0') {
-    //do nothing
-    return
+  if (buffer === "0") {
+    // do nothing
+    return;
   }
-  const intBuffer = parseInt(buffer);
+
+  let intBuffer = parseFloat(buffer);
+
+  if (buffer[0] === "-") {
+    intBuffer = -intBuffer;
+  }
+
   if (runningTotal === 0) {
     runningTotal = intBuffer;
   } else {
@@ -73,20 +99,25 @@ function handleMath(symbol) {
   }
 
   previousOperator = symbol;
-  buffer = '0';
+  buffer = "0";
 }
 
 function calculate(intBuffer) {
-  if (previousOperator === '+') {
+  if (previousOperator === "+") {
     runningTotal += intBuffer;
-  } else if (previousOperator === '-') {
+  } else if (previousOperator === "-") {
     runningTotal -= intBuffer;
-  } else if (previousOperator === 'x') {
+  } else if (previousOperator === "x") {
     runningTotal *= intBuffer;
-  } else if (previousOperator === '÷') {
+  } else if (previousOperator === "÷") {
+    if (intBuffer === 0) {
+      buffer = "Error: Division by 0";
+      runningTotal = 0;
+      previousOperator = null;
+      return;
+    }
     runningTotal /= intBuffer;
   }
-
 }
 
 function init() {
