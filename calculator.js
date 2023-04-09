20fc9b5c9043f8cd98c98a03a0c1a63ba0510a6e
Thanh Nguyen <81922015+nhthanh719@users.noreply.github.com>
2023-04-09 16:12:49 +0700

    Revert "Fix multiply & divide operator"

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ let previousOperator = null;
 const screen = document.querySelector("#screen");
 
 function buttonClick(value) {
-  if (isNaN(parseFloat(value))) {
+  if (isNaN(parseInt(value))) {
     handleSymbol(value);
   } else {
     handleNumber(value);
@@ -13,85 +13,59 @@ function buttonClick(value) {
 }
 
 function handleNumber(number) {
-  if (buffer === "0") {
+  if (buffer === '0') {
     buffer = number;
   } else {
     buffer += number;
   }
-  buffer = parseFloat(buffer).toString();
 }
 
 function handleSymbol(symbol) {
   switch (symbol) {
-    case "C":
-      buffer = "0";
-      runningTotal = 0;
+    case 'C':
+      buffer = '0';
       previousOperator = null;
       break;
 
-    case "=":
+    case '=':
       if (previousOperator === null) {
-        // Need numbers to do math
+        //Need numbers to do math
         return;
-      }
-      console.log(runningTotal);
-      calculate(parseFloat(runningTotal));
-      buffer = "" + runningTotal;
+      } else {
+
+        calculate(parseInt(buffer));
+        buffer = "" + runningTotal; 
+      } 
+      
       runningTotal = 0;
       previousOperator = null;
       break;
 
-    case "←":
+
+    case '←':
       if (buffer.length === 1) {
-        buffer = "0";
+        buffer = '0';
       } else {
         buffer = buffer.substring(0, buffer.length - 1);
       }
       break;
 
-    case "+":
-    case "-":
-      if (previousOperator === null) {
-        previousOperator = symbol;
-        runningTotal = parseFloat(buffer);
-      } else {
-        calculate(parseFloat(buffer));
-        previousOperator = symbol;
-      }
-      buffer = "0";
-      break;
-
-    case "x":
-    case "÷":
-      if (previousOperator === null) {
-        previousOperator = symbol;
-        runningTotal = parseFloat(buffer);
-      } else {
-        // Check if a number has been entered before clicking the operator
-        if (buffer === "0") {
-          previousOperator = symbol;
-        } else {
-          calculate(parseFloat(buffer));
-          previousOperator = symbol;
-        }
-      }
-      buffer = "0";
+    case '+':
+    case '-':
+    case 'x':
+    case '÷':
+      handleMath(symbol);
+      buffer = '0';
       break;
   }
 }
 
 function handleMath(symbol) {
-  if (buffer === "0") {
-    // do nothing
-    return;
+  if (buffer === '0') {
+    //do nothing
+    return
   }
-
-  let intBuffer = parseFloat(buffer);
-
-  if (buffer[0] === "-") {
-    intBuffer = -intBuffer;
-  }
-
+  const intBuffer = parseInt(buffer);
   if (runningTotal === 0) {
     runningTotal = intBuffer;
   } else {
@@ -99,25 +73,20 @@ function handleMath(symbol) {
   }
 
   previousOperator = symbol;
-  buffer = "0";
+  buffer = '0';
 }
 
 function calculate(intBuffer) {
-  if (previousOperator === "+") {
+  if (previousOperator === '+') {
     runningTotal += intBuffer;
-  } else if (previousOperator === "-") {
+  } else if (previousOperator === '-') {
     runningTotal -= intBuffer;
-  } else if (previousOperator === "x") {
+  } else if (previousOperator === 'x') {
     runningTotal *= intBuffer;
-  } else if (previousOperator === "÷") {
-    if (intBuffer === 0) {
-      buffer = "Error: Division by 0";
-      runningTotal = 0;
-      previousOperator = null;
-      return;
-    }
+  } else if (previousOperator === '÷') {
     runningTotal /= intBuffer;
   }
+
 }
 
 function init() {
